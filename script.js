03a29fceb86ccdd16a7775a4bd7c79238b2029e6
Serafino <gourav1315.cse18@chitkara.edu.in>
2023-04-02 18:11:22 +0530

    quick comit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+const learnMoreBtn = document.querySelector('.btn');
+
+// Add a click event listener to the button
+learnMoreBtn.addEventListener('click', function() {
+  // Redirect the user to the Learn More page
+  window.location.href = 'https://retreatcottage.in/learn-more.html';
+});
